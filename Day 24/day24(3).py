dea0d993876d3a79d9224290c478b709c39e9ad9
DAVITSTEPNIASHVILI <dstepniashvili@gmail.com>
2025-01-04 19:31:31 +0400

    commit

---
+++ Day 24/day24(3).py
@@ -0,0 +1,12 @@
+gt=int(input("Enter number: "))
+if gt>0:
+    print("positive")
+else:
+    print("negative")
+
+
+hu=int(input("Enter number: "))
+if hu>100:
+    print("big number")
+else:
+    print("small number")
