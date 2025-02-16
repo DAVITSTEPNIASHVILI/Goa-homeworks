83143242227a7839d530152f66b8c630cf8900fc
DAVITSTEPNIASHVILI <dstepniashvili@gmail.com>
2025-02-16 14:35:14 +0400

    commit

---
+++ Day 36/Homework/day36.py
@@ -0,0 +1,90 @@
+# find
+print("Phyton".find("P"))
+
+user_information = input("Enter information: ")
+print(user_information.find(user_information[0]))
+
+
+def find_character_index(string, char):
+    print(string.find(char))
+
+find_character_index("Phyton", "y")
+
+# count
+def strings (string):
+    string = string.split()
+    print(string.count("the"))
+
+strings("i have the book, the table, the notebook")
+
+def string():
+    user_information2 = input("Enter information: ")
+    my_print = "ujbhj"
+
+    print(user_information2.count(my_print))
+
+string()
+
+def string2(string, char):
+    print(string.find(char))
+
+string2("kodjbahEF", "GIUFIDSUJKFMDSSKOJADFSMS")
+
+# index
+def strings4():
+    print("hello".index("h"))
+
+strings4()
+
+def string8():
+    user_informations = input("Enter information: ")
+    my_print3 = "print"
+    print(user_informations.index(my_print3))
+
+string8()
+
+# Islower
+def islower():
+    print("LapTop".islower())
+
+islower()
+
+def islower2():
+    print1 = "Hello World!"
+    if print1 == print1.islower():
+        print(True)
+    else:
+        print(False)
+
+islower2()
+
+def islower3():
+    user_informations2 = input("Enter information: ")
+    print(user_informations2.islower())
+
+islower3()
+
+# Isupper
+def Check_string():
+    user_information8 = input("Enter information: ")
+    print(user_information8.isupper())
+
+Check_string()
+
+def Check_string2():
+    user_information9 = input("Enter information: ")
+    if user_information9 == user_information9.isupper():
+        print(True)
+    else:
+        print(False)
+
+Check_string2()
+
+def Check_string3():
+    user_information6 = input("Enter information: ")
+    if user_information6 == user_information6.isupper():
+        print("your information is in uppercase")
+    else:
+        print("your information isn't in uppercase")
+
+Check_string2()
