c4810194975e0044ba3586f8a080b8ee5153a3e2
DAVITSTEPNIASHVILI <dstepniashvili@gmail.com>
2025-03-28 22:09:15 +0400

    commit

---
+++ Day 50/Classwork/day50.py
@@ -0,0 +1,9 @@
+satamasho_bekeka = {
+    "name":"bekeka",
+    "color":"white and black",
+    "price":30
+}
+keys = satamasho_bekeka.keys()
+values = satamasho_bekeka.values()
+items = satamasho_bekeka.items()
+print(keys, values, items)
