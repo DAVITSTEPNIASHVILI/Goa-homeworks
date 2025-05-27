d57d3176c51a1b3178f5bb08ecaccc43dfa50e94
DAVITSTEPNIASHVILI <dstepniashvili@gmail.com>
2025-05-27 10:32:17 +0400

    commit

---
+++ Day 67/Homework/main.js
@@ -0,0 +1,20 @@
+// Create an empty object.
+emptyObject = {
+
+}
+// Create an object with your name, age, and city.
+me = {
+    name : "David",
+    age : 11,
+    city : "Tbilisi"
+}
+// Access the value of a property in an object.
+console.log(me["age"])
+// Add a new property to an existing object.
+me.age = 12
+console.log(me)
+// Create a nested object (an object inside another object).
+console.log(me.name)
+// Change the value of an existing property in an object.
+me.name = "daviti"
+console.log(me)
