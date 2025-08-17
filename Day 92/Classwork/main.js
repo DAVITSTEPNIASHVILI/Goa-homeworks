fc7a4907f8a10306c5064a930d5dbd8dea875cc9
DAVITSTEPNIASHVILI <dstepniashvili@gmail.com>
2025-08-17 12:44:23 +0400

    commit

---
+++ Day 92/Classwork/main.js
@@ -0,0 +1,11 @@
+const color = ["green","red","blue"]
+console.log(color.indexOf("blue"))
+
+const word = ["Javascript is programming language"]
+console.log(word.indexOf("Javascript"))
+
+const nums = [12,21,24,42,54,42,65,75]
+console.log(nums.lastIndexOf(42))
+
+const longString = ["my name is david"]
+console.log(longString.lastIndexOf("a"))
---
+++ Day 92/Homework/main.js
@@ -0,0 +1,10 @@
+// 2) In an array of fruits, find the first index of "apple" starting from position 3.
+const fruits = ["apple","banana","apple","mango","apple"]
+const fruits2 = fruits[2,4]
+console.log(fruits2.indexOf("apple"))
+// 3) Given a sentence, find the index of the last occurrence of the letter "e".
+const sentence = ["Hello my name is David."]
+console(sentence.lastIndexOf("e"))
+// 4) Given a string, find the index of the first space character.
+const string1 = ["My name is David."]
+console.log(string1.indexOf(" "))
