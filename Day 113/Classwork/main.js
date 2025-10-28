7727bca4ab9ac3f3c7c22d785cf4e3148d9f0fe1
DAVITSTEPNIASHVILI <dstepniashvili@gmail.com>
2025-10-28 21:14:21 +0400

    commit

---
+++ Day 113/Classwork/main.js
@@ -0,0 +1,25 @@
+// const p1 = <p>hello</p>
+// const h1 = <h1>hello</h1>
+// const h2 = <h2>hello</h2>
+
+// საკლასო დავალება:
+
+// შექმენით ერთი ცვლადი, რომელშიც შეინახავთ jsx-ის ელემენტს და ამ ელემენტში მთლიანად უნდა ინახებოდეს მოცემული სტრუქტურა:
+// const Website = (
+//     <div>
+//         <h1>My First Webpage</h1>
+//         <p>Hi, my name is Henry and this is my first web-site. I am interested in:</p>
+//         <ul>
+//             <li>Web design</li>
+//             <li>Coding</li>
+//             <li>Digital art</li>
+//         </ul>
+//     </div>
+// );
+
+// const Website2 = (
+//     <div>
+//         <h1>My First Heading</h1>
+//         <p>My First Paragraph</p>
+//     </div>
+// );
