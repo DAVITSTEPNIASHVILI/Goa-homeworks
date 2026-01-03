a73c9f0bc2cb6c3723815e54b3ccc9438367456e
DAVITSTEPNIASHVILI <davitstepniashvili4@gmail.com>
2026-01-03 15:34:15 +0400

    commit

---
+++ react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./components/Main"
+import NavBar from "./components/NavBar"
+
+
+const App = () => {
+  return (
+    <div className="w-full h-screen">
+      <NavBar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
