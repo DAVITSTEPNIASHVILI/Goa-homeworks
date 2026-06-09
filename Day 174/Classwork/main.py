6db3c7cdd680a38ba528b843429651ff9b327129
DAVITSTEPNIASHVILI <davitstepniashvili4@gmail.com>
2026-06-09 18:22:30 +0400

    commit

---
+++ Day 174/Classwork/main.py
@@ -0,0 +1,9 @@
+# codewars
+# https://www.codewars.com/kata/514b92a657cdc65150000006/train/python
+
+def solution(number):
+    res=0
+    for i in range(number):
+        if i % 3 == 0 or i % 5 == 0:
+            res += i
+    return res
